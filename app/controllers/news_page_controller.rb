c17a30130e51a713e360572baa18b7c95e05e1c4
haminata <amtavcc@hotmail.com>
2016-07-27 21:15:56 +0100

    - added links

---
+++ app/controllers/news_page_controller.rb
@@ -0,0 +1,24 @@
+class NewsPageController < ApplicationController
+def index
+  #@news_page = News_page.all
+end
+
+  def politics
+  end
+
+  def business
+  end
+
+def technology
+
+end
+
+  def entertainment
+  end
+
+def weather
+
+end
+
+
+end
