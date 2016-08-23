8b3b5e8966fa5e017de765e128b9bab90b5c6001
haminata <amtavcc@hotmail.com>
2016-08-23 08:03:22 +0100

    added images for homepage created a form for sign in created a table for forum

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,6 +1,13 @@
 class HomePageController < ApplicationController
 def index
+  @imageNames = []#["syria.jpg", "kenyauni.jpg", "GAMBIA.jpeg"]
 
+  @articles = NewsArticle.all()
+
+  @articles.each do |article|
+    @imageNames.append(article.image_url)
+
+  end
 end
 end
 
